978f85cf108bada276e638e09ba072504de12d25
justindglee <107393984+justin-chopsticks@users.noreply.github.com>
2023-02-03 12:01:58 +0900

    Add jsdoc

---
+++ controller.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Todo item
+ * @class
+ *
+ */
+class Todo {
+  /**
+   *
+   * Initialize a new instance of Todo
+   * @param {TodoModel} todo
+   *
+   */
+  constructor(todo) {
+    this.todo = todo;
+  }
+
+  /**
+   *
+   * @method create
+   * @method read
+   * @method update
+   * @method delete
+   *
+   */
+}
--- models.js
+++ models.js
@@ -8,11 +8,11 @@ class Todo {
 
   /**
    *
-   * @param {number} id
-   * @param {string} contents
-   * @param {boolean} completed
-   * @param {string} category
-   * @param {Array.<Tags>} [tags]
+   * @param {number} id identifier
+   * @param {string} contents contents of a todo item
+   * @param {boolean} completed Whether todo is completed or not
+   * @param {string} category Category of a todo item
+   * @param {Array.<Tags>} [tags] Tags for a todo item
    */
   constructor(id, contents, completed, category, tags) {}
 }
